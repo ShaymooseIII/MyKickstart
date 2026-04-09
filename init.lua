2b1c510d90b17015f12c944485d55ed0c7fbdbdf
Geimus <151066590+ShaymooseIII@users.noreply.github.com>
2026-04-09 01:31:39 -0700

    added shortcut to open terminal at bottom and fixed working directory issues when opening terminal

--- init.lua
+++ init.lua
@@ -110,6 +110,9 @@ vim.o.mouse = 'a'
 -- Don't show the mode, since it's already in the status line
 vim.o.showmode = false
 
+-- Neovim automatically follows file directory(makes terminal open correctly in right directory)
+vim.opt.autochdir = true
+
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
@@ -217,6 +220,9 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+
+-- key map to open a split terminal at the bottom of current file
+vim.keymap.set('n', '<leader>T', function() vim.cmd 'belowright 12split | terminal' end, { desc = 'Bottom terminal' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
