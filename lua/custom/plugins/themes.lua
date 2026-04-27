d422e8e78480f5760ee7efce4684307bf472f6c2
Geimus <151066590+ShaymooseIII@users.noreply.github.com>
2026-04-26 17:42:38 -0700

    midnight not loading initially for default theme

--- lua/custom/plugins/themes.lua
+++ lua/custom/plugins/themes.lua
@@ -2,6 +2,10 @@
 ---@type LazySpec
 -- LazySpec (plugin specification)
 return {
-  { 'dasupradyumna/midnight.nvim', lazy = false, priority = 1000 },
+  { 'dasupradyumna/midnight.nvim', lazy = false, priority = 1000, 
+config = function()
+    vim.cmd.colorscheme 'midnight'
+  end
+ },
   -- `lazy` and `priority` are only needed if this is your primary colorscheme to load it first
 }
